f3702c96189b196e4a79f9cf93e13dda177ec007
joaovitor <joaovitorvlb@hotmail.com>
2017-08-10 03:29:03 +0000

    Built packet processing

--- SolarIotControl-01.py
+++ SolarIotControl-01.py
@@ -26,12 +26,20 @@ while True:
         print 'Received from ', add[0], ' >> ', data
         data = data.strip()
         tag_pct, i_00, i_01, i_02, i_03 = data.split(':')
-        print 'Desmenbrando pacote'
-        print 'Tag do pacote ', tag_pct
-        print 'Load device 1 ', i_00, ' mA'
-        print 'Load device 2 ', i_01, ' mA'
-        print 'Load device 3 ', i_02, ' mA'
-        print 'Load device 4 ', i_03, ' mA'
+        if tag_pct == 'ESP01-IIII':
+            print 'Processing package'
+            print 'Package tag   ', tag_pct
+            print 'Load device 1 ', i_00, ' mA'
+            print 'Load device 2 ', i_01, ' mA'
+            print 'Load device 3 ', i_02, ' mA'
+            print 'Load device 4 ', i_03, ' mA'
+        if tag_pct == 'ESP02-VVVV':
+            print 'Processing package'
+            print 'Package tag       ', tag_pct
+            print 'Device 01 voltage ', i_00, ' V'
+            print 'Device 02 voltage ', i_01, ' V'
+            print 'Device 03 voltage ', i_02, ' V'
+            print 'Devide 04 voltage ', i_03, ' V' 
 
         cont = cont + 1
 server.close()
